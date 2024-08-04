a374cfee117a1429c417371d5894b2eaded0fbd7
yukenz <yuyun.purniawan@gmail.com>
2024-08-04 21:39:28 +0700

    Update : change LZ4CompressionAlgorithm inheritance from Interface to Abstract

--- src/main/java/id/awan/jee/util/LZ4CompressionAlgorithm.java
+++ src/main/java/id/awan/jee/util/LZ4CompressionAlgorithm.java
@@ -1,29 +1,47 @@
 package id.awan.jee.util;
 
+import io.jsonwebtoken.impl.compression.AbstractCompressionAlgorithm;
 import io.jsonwebtoken.io.CompressionAlgorithm;
 import net.jpountz.lz4.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class LZ4CompressionAlgorithm implements CompressionAlgorithm {
+public class LZ4CompressionAlgorithm extends AbstractCompressionAlgorithm {
 
     LZ4Factory factory = LZ4Factory.fastestInstance();
 
+    public LZ4CompressionAlgorithm() {
+        super("LZ4");
+    }
+
+//    @Override
+//    public OutputStream compress(OutputStream out) {
+//        LZ4Compressor compressor = factory.fastCompressor();
+//        return new LZ4BlockOutputStream(out, 64 * 1024, compressor);
+//    }
+
     @Override
-    public OutputStream compress(OutputStream out) {
+    protected OutputStream doCompress(OutputStream outputStream) throws IOException {
         LZ4Compressor compressor = factory.fastCompressor();
-        return new LZ4BlockOutputStream(out, 64 * 1024, compressor);
+        return new LZ4BlockOutputStream(outputStream, 64 * 1024, compressor);
     }
 
+//    @Override
+//    public InputStream decompress(InputStream in) {
+//        LZ4FastDecompressor lz4FastDecompressor = factory.fastDecompressor();
+//        return new LZ4BlockInputStream(in, lz4FastDecompressor);
+//    }
+
     @Override
-    public InputStream decompress(InputStream in) {
+    protected InputStream doDecompress(InputStream inputStream) throws IOException {
         LZ4FastDecompressor lz4FastDecompressor = factory.fastDecompressor();
-        return new LZ4BlockInputStream(in, lz4FastDecompressor);
+        return new LZ4BlockInputStream(inputStream, lz4FastDecompressor);
     }
 
-    @Override
-    public String getId() {
-        return "LZ4";
-    }
+//    @Override
+//    public String getId() {
+//        return "LZ4";
+//    }
 }
